a6c754732eb7d800ab4c3b350503c722b8a2ad37
-FredSouza- <72362780+3koi-pip@users.noreply.github.com>
2020-12-17 16:20:06 -0300

    Update Ep 6 - contours.py

--- Material de estudo/Seção 1/Codigos de Apoio/Ep 6 - contours.py
+++ Material de estudo/Seção 1/Codigos de Apoio/Ep 6 - contours.py
@@ -9,22 +9,22 @@ cv.imshow('Cats', img)
 blank = np.zeros(img.shape, dtype='uint8')
 cv.imshow('Blank', blank)
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # colocando em preto-branco
 cv.imshow('Gray', gray)
 
-blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
-cv.imshow('Blur', blur)
+#blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT) # se vc usar o borramento o número de contornos tende a cair, pq vc "eliminou" algumas bordas
+#cv.imshow('Blur', blur)
 
-canny = cv.Canny(blur, 125, 175)
+canny = cv.Canny(img, 125, 175) # descobrindo as "bordas"(edges)
 cv.imshow('Canny Edges', canny)
 
-# ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
+# ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY) # binarização da imagem (branco=1/preto=0)
 # cv.imshow('Thresh', thresh)
 
-contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE) # contornos, hierarquia (entre os contornos) = (função borda, cv.metodo de retorno, cv.metodo de aproximação)
 print(f'{len(contours)} contour(s) found!')
 
-cv.drawContours(blank, contours, -1, (0,0,255), 1)
+cv.drawContours(blank, contours, -1, (255,255,255), 1) # desenho dos contornos da imagem
 cv.imshow('Contours Drawn', blank)
 
 cv.waitKey(0)
