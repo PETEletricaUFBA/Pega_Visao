e5bb129a6a1460229e277555b78871ef46a220d2
-FredSouza- <72362780+3koi-pip@users.noreply.github.com>
2020-12-17 16:18:33 -0300

    Update Ep 5 - transformations.py

--- Material de estudo/Seção 1/Codigos de Apoio/Ep 5 - transformations.py
+++ Material de estudo/Seção 1/Codigos de Apoio/Ep 5 - transformations.py
@@ -6,48 +6,52 @@ import numpy as np
 img = cv.imread('Photos/park.jpg')
 cv.imshow('Park', img)
 
-# Translation
-def translate(img, x, y):
+# para fazer transformações deveremos criar algumas funções a parte a biblioteca
+
+#Translação
+def translate(img,x,-y):
     transMat = np.float32([[1,0,x],[0,1,y]])
-    dimensions = (img.shape[1], img.shape[0])
-    return cv.warpAffine(img, transMat, dimensions)
-
-# -x --> Left
-# -y --> Up
-# x --> Right
-# y --> Down
-
+    dimensions = (img.shape[1], img.shape[0]) # img.shape[1] = largura img.shap[0] = altura
+    return cv.warpAffine(img,transMat,dimensions)
+# x --> direita
+# -x --> esquerda
+# y --> acima
+# -y --> abaixo
+# vale notar que o deslocamento na verdade é uma adição de pixels em brano a imagem
 translated = translate(img, -100, 100)
 cv.imshow('Translated', translated)
 
-# Rotation
-def rotate(img, angle, rotPoint=None):
-    (height,width) = img.shape[:2]
+#Rotação
+def rotate(img,angle,rotPoint=None):
+    (altura,largura) = img.shape[:2]
 
     if rotPoint is None:
-        rotPoint = (width//2,height//2)
-    
-    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
-    dimensions = (width,height)
+        rotPoint = (largura//2,altura//2)#centro da imagem
 
-    return cv.warpAffine(img, rotMat, dimensions)
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)# esse 1.0 indica se queremos aumentar a imagem dps da rotação
+    dimensions = (largura,altura)
 
+    return cv.warpAffine(img,rotMat,dimensions)
+# a rotação acontece no sentido anti-horário para valores positivos
 rotated = rotate(img, -45)
 cv.imshow('Rotated', rotated)
 
 rotated_rotated = rotate(img, -90)
 cv.imshow('Rotated Rotated', rotated_rotated)
 
-# Resizing
+# Resizing igual ao arquivo img_resize.py
 resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
 cv.imshow('Resized', resized)
 
-# Flipping
-flip = cv.flip(img, -1)
+# Flip/espelhamento
+flip = cv.flip(img, 0)
 cv.imshow('Flip', flip)
+# 0 = espelha imagem em torno do eixo x
+# 1 = espelha em torno do eixo y
+# -1 = espelha em torno de ambos os eixos
 
-# Cropping
-cropped = img[200:400, 300:400]
+# Cropping/Corte
+cropped = img[200:400, 300:400] #indique os pontos onde deseja cortar a imagem
 cv.imshow('Cropped', cropped)
 
 
