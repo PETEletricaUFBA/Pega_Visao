f4e5f1a69f5ee055029c33d4a13dccd193270809
3fred3 <72362780+3fred3@users.noreply.github.com>
2021-04-15 21:35:33 -0300

    Create Webcam_Fotos.py
    Aplicação de todos os conhecimentos presentes nas secções 2, uso de webcam, normalização, salvar fotos

---
+++ Aplicações/Webcam_Fotos.py
@@ -0,0 +1,37 @@
+import cv2
+from time import sleep
+key = cv2. waitKey(1)
+webcam = cv2.VideoCapture(0)
+sleep(2)
+while True:
+
+    try:
+        check, frame = webcam.read()
+        #print(check) #prints true se webcam está ativa
+        #print(frame) #prints matrix de cada frame
+        cv2.imshow("Capturing", frame)
+        key = cv2.waitKey(1)
+        if key == ord('s'):
+            cv2.imwrite(filename='saved_img.jpg', img=frame)
+            webcam.release()
+
+            img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
+
+            gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+            img_ = cv2.resize(gray,(28,28))
+
+            img_resized = cv2.imwrite(filename='saved_img-normalized.jpg', img=img_)
+            print("Image saved!")
+
+            break
+
+        elif key == ord('q'):
+            webcam.release()
+            cv2.destroyAllWindows()
+            break
+
+    except(KeyboardInterrupt):
+        webcam.release()
+        print("Program ended.")
+        cv2.destroyAllWindows()
+        break
